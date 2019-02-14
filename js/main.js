17fdd376481d7795c1f73e45e0fde97f135fd821
Ivan5 <converse_ivan@hotmail.com>
2019-02-14 15:32:00 -0600

    Add show pdf page

--- js/main.js
+++ js/main.js
@@ -10,13 +10,60 @@ const canvas = document.getElementById("pdf-render");
 const ctx = canvas.getContext("2d");
 
 //Render the page
-const renderPAge = num => {
+const renderPage = num => {
   pageIsRendering = true;
 
   //Get page
   pdfDoc.getPage(num).then(page => {
-    console.log(page);
+    //Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    //Output current page
+    document.querySelector("#page-num").textContent = num;
   });
+};
+
+//Check for pages rendering
+const queueRenderPage = num => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+//Show Prev Page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+//Show next Page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+
+  pageNum++;
+  queueRenderPage(pageNum);
 };
 
 //Get Document
@@ -26,3 +73,7 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
 
   renderPAge(pageNum);
 });
+
+// Button events
+document.querySelector("#prev-page").addEventListener("click", showPrevPage);
+document.querySelector("#next-page").addEventListener("click", showNextPage);
